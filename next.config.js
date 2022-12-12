ef5e7feacbc83fccd798e0e362fd9bb82dc33cfb
Roman Mashin <roman-mashin@yandex.ru>
2022-12-12 09:51:08 +0500

    added pwa

--- next.config.js
+++ next.config.js
@@ -11,3 +11,12 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
+
+module.exports = withPWA({
+  // config
+});
